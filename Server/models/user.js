19ae0bf412430e1bf749905ef4bfbf8fc0ef15b3
Brayan Alberto Castellanos Bernal <brayancastellanos24@outlook.com>
2022-08-29 15:52:27 -0500

    update

---
+++ Server/models/user.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require("sequelize");
+
+const User = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+    field: 'id_usuario',
+  },
+  nombre: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    field: 'nombre',
+  },
+  apellido: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    field: 'apellido',
+  },
+  document:{
+    allowNull:false,
+    type: DataTypes.STRING,
+    field: 'documento',
+  },
+  telefono:{
+    allowNull:false,
+    type: DataTypes.STRING,
+    field: 'telefono',
+  },
+  correo: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    unique: true,
+    field: 'correo',
+  },
+  contraseña: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    unique: true,
+    field: 'contrasena'
+  }
+};
+
+module.exports = User;
