8c061d243a8e2faa06d6a25a44e6b93c456e224b
brayan castellanos <brayancastellanos24@outlook.com>
2022-09-06 23:07:59 -0500

    update

--- Server/routers/routerDoc.js
+++ Server/routers/routerDoc.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const DocController =  require("../controllers/TipoDoc/tipoDoc-get.controller.js");
-const putDocController = require("../controllers/TipoDoc/tipoDoc-post.controller.js")
-const deleteDocController = require("../controllers/TipoDoc/tipoDoc-delete.controller.js");
-const updateDocController =  require("../controllers/TipoDoc/tipoDoc-update.controller.js")
+// const DocController =  require("../controllers/TipoDoc/tipoDoc-get.controller.js");
+// const putDocController = require("../controllers/TipoDoc/tipoDoc-post.controller.js")
+// const deleteDocController = require("../controllers/TipoDoc/tipoDoc-delete.controller.js");
+// const updateDocController =  require("../controllers/TipoDoc/tipoDoc-update.controller.js")
+
+const {getDoc, getDocById, deleteDoc, postDoc, updateDoc} =  require("../controllers/TipoDoc/index.js");
 
 const api =  express.Router();
 
-api.get("/List-Doc", DocController.getDoc);
-api.post("/Create-Doc",putDocController.postDoc);
-api.get("/List-Doc/:id",DocController.getDocById);
-api.delete("/Delete-Doc/:id",deleteDocController.deleteDoc);
-api.put("/Update-Doc/:id",updateDocController.updateDoc);
+api.get("/List-Doc", getDoc);
+api.post("/Create-Doc",postDoc);
+api.get("/List-Doc/:id",getDocById);
+api.delete("/Delete-Doc/:id",deleteDoc);
+api.put("/Update-Doc/:id",updateDoc);
 
 module.exports = api;
