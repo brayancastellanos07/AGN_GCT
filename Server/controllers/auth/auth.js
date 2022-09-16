8854ebfa5bb73033a7c13cf5aa6ad9b580af40ad
Brayan Alberto Castellanos Bernal <brayancastellanos24@outlook.com>
2022-09-16 12:55:28 -0500

    update

--- Server/controllers/auth/auth.js
+++ Server/controllers/auth/auth.js
@@ -1,30 +1,54 @@
-const jwt = require('../../services/jwt');
+const jwt = require("../../services/jwt");
 const moment = require("moment");
-const Usuario = require("../../models/user"); 
+const Usuario = require("../../models/user");
+const colors = require("colors");
 
-function willExpiredToken(token){
-    // decodificación del token 
-    const {exp} =  jwt.decodeToken(token);   
-    const currentDate = moment().unix();
-    
-    // si la fecha actual es mayor a la fecha de vencimiento del token 
-    //  retorna un true 
-    // de lo contraio retorna un false 
-    if (currentDate > exp) {
-        return true;
-    }
-    return false;
+function willExpiredToken(token) {
+  // decodificación del token
+  const { exp } = jwt.decodeToken(token);
+  const currentDate = moment().unix();
+
+  // si la fecha actual es mayor a la fecha de vencimiento del token
+  //  retorna un true
+  // de lo contraio retorna un false
+  if (currentDate > exp) {
+    return true;
+  }
+  return false;
 }
 
-function refreshAccessToken(req, res){
-    const {refreshToken} =  req.body;
-    const isTokenExpired = willExpiredToken(refreshToken);
-    
-    if (isTokenExpired) {
-        res.status(404).send({message: "El refreshToken ha expirado"}); 
-    }else{
-        //Usuario.findOne()
+async function refreshAccessToken(req, res) {
+  const { refreshToken } = req.body;
+  const isTokenExpired = willExpiredToken(refreshToken);
+
+  if (isTokenExpired) {
+    res.status(404).send({ message: "El refreshToken ha expirado" });
+  } else {
+    const { id } = jwt.decodeToken(refreshToken);
+
+    const data = await Usuario.findOne({ where: { id: id } });
+
+    //return res.status(400).json({data})
+
+    try {
+      if (!data) {
+        return res.status(404).send(`No se encontro el  Usuario ${id}`);
+      }
+      return res.status(400).json({data})
+    //   return res
+    //     .status(200)
+    //     .send({
+    //       accessToken: jwt.createAccessToken(data),
+    //       refreshToken: refreshToken,
+    //     });
+    } catch (error) {
+      console.log(
+        colors.red({ message: "Error en refreshAccessToken" }),
+        error
+      );
+      return res.status(500).send({ message: "Error en el servidor" });
     }
+  }
 }
 
-module.exports = {refreshAccessToken};
+module.exports = { refreshAccessToken };
